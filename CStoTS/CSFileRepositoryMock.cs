076f55dcb45e47a84d6499ec6255951633622531
kazenetu <rakudafactory@gmail.com>
2019-07-16 23:54:26 +0900

    classのTS変換の実装検証を追加

---
+++ CStoTS/CSFileRepositoryMock.cs
@@ -0,0 +1,75 @@
+﻿using CSharpAnalyze.Domain.PublicInterfaces.Events;
+using CSharpAnalyze.Domain.PublicInterfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CStoTS
+{
+  /// <summary>
+  /// テスト用FileRepository
+  /// </summary>
+  public class CSFileRepositoryMock : ICSFileRepository
+  {
+    /// <summary>
+    /// ファイルリスト
+    /// </summary>
+    private List<(string relativePath, string source)> files = new List<(string relativePath, string source)>();
+
+    /// <summary>
+    /// メソッドリスト
+    /// </summary>
+    /// <remarks>ファイルに紐づくメソッドのりスト</remarks>
+    private List<(string relativePath, Action<IAnalyzed> delegateMethod)> actions = new List<(string relativePath, Action<IAnalyzed> delegateMethod)>();
+
+    /// <summary>
+    /// ファイルリスト取得
+    /// </summary>
+    /// <param name="rootPath">対象ファイルルート(未使用)</param>
+    /// <param name="exclusionKeywords">除外ファイルリスト(未使用)</param>
+    /// <returns>ファイルリスト</returns>
+    public List<(string relativePath, string source)> GetCSFileList(string rootPath, List<string> exclusionKeywords)
+    {
+      return files;
+    }
+
+    /// <summary>
+    /// ファイル情報追加
+    /// </summary>
+    /// <param name="filePath">ファイル名</param>
+    /// <param name="source">ソースコード</param>
+    /// <param name="delegateMethod">メソッド</param>
+    public void Add(string filePath, string source, Action<IAnalyzed> delegateMethod = null)
+    {
+      files.Add((filePath, source));
+      if (delegateMethod != null)
+      {
+        actions.Add((filePath, delegateMethod));
+      }
+    }
+
+    /// <summary>
+    /// メソッドを取得する
+    /// </summary>
+    /// <param name="filePath">ファイル名</param>
+    /// <returns>メソッドまたはnull(未定義)</returns>
+    public Action<IAnalyzed> GetDelegateMethod(string filePath)
+    {
+      var target = actions.Where(action => action.relativePath == filePath);
+
+      if (!target.Any()) return null;
+
+      return target.First().delegateMethod;
+    }
+
+    /// <summary>
+    /// ファイル情報クリア
+    /// </summary>
+    public void Clear()
+    {
+      files.Clear();
+      actions.Clear();
+    }
+  }
+}
--- CStoTS/Class1.cs
+++ CStoTS/Class1.cs
@@ -1,8 +1,59 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 namespace CStoTS
 {
   public class Class1
   {
+    /// <summary>
+    /// Entry Point Method
+    /// </summary>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    static int Main(string[] args)
+    {
+      
+      var csApplication = new CSharpAnalyze.ApplicationService.AnalyzeApplication();
+
+      // Register Analyzed Event
+      csApplication.Register<CSharpAnalyze.Domain.PublicInterfaces.Events.IAnalyzed>(csApplication, (ev) =>
+      {
+        var class1 = new Class1();
+        var result = class1.ConvertTS(ev);
+        Console.WriteLine(result);
+      });
+
+      // C# Source Code
+      var testBase = new TestBase();
+      testBase.CreateFileData("test", string.Empty, @"public class Test
+      {
+      }", null);
+
+
+      // Run C# Analyze
+      csApplication.Analyze(string.Empty, testBase.Files);
+
+#if DEBUG
+      Console.ReadKey();
+#endif
+
+      return 0;
+    }
+
+    string ConvertTS(CSharpAnalyze.Domain.PublicInterfaces.Events.IAnalyzed analyzed){
+      return ConvertTS((dynamic)analyzed.FileRoot.Members.First());
+    }
+
+    string ConvertTS(CSharpAnalyze.Domain.PublicInterfaces.AnalyzeItems.IItemClass item, int indent = 0)
+    {
+      var result = new StringBuilder();
+      var indentSpace = " ";
+
+      result.AppendLine($"{indentSpace}class {item.Name}{{");
+      result.AppendLine($"{indentSpace}}}");
+
+      return result.ToString();
+    }
   }
 }

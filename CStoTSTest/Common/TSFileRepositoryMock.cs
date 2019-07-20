64ca8d60ed374eb44fb7672ab412ebb4f9d41453
kazenetu <rakudafactory@gmail.com>
2019-07-21 00:23:01 +0900

    テスト用モックファイルを追加

---
+++ CStoTSTest/Common/TSFileRepositoryMock.cs
@@ -0,0 +1,19 @@
+﻿using CStoTS.Infrastructure;
+using System;
+
+namespace CStoTSTest.Common
+{
+  internal class TSFileRepositoryMock: ITSFileRepository
+  {
+    /// <summary>
+    /// TypeScriptの出力
+    /// </summary>
+    /// <param name="filePath">出力パス</param>
+    /// <param name="tsData">変換後のTypeScript文字列</param>
+    public void WriteFile(string filePath, string tsData)
+    {
+      Console.WriteLine($"------{filePath}------");
+      Console.Write(tsData);
+    }
+  }
+}

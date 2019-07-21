57cf632efd06fc193fbc566819bd5b2259fbfc90
kazenetu <rakudafactory@gmail.com>
2019-07-22 08:25:55 +0900

    実行テストをテストケースに一旦実装

--- CStoTSTest/UnitTest1.cs
+++ CStoTSTest/UnitTest1.cs
@@ -1,4 +1,6 @@
-using System;
+using CStoTS;
+using CStoTS.ApplicationService;
+using CStoTSTest.Common;
 using Xunit;
 
 namespace CStoTSTest
@@ -8,7 +10,18 @@ namespace CStoTSTest
     [Fact]
     public void Test1()
     {
+      // 一旦実装
 
+      // C#ソース作成
+      var testBase = new TestBase();
+      testBase.CreateFileData("test.cs", string.Empty,
+      @"public class Test
+      {
+      }", null);
+
+      // 変換・出力
+      var csToTs = new ConvertApplication();
+      csToTs.Convert(string.Empty, string.Empty, new TSFileRepositoryMock(), testBase.Files);
     }
   }
 }

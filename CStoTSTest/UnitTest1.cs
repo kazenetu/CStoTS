e2092c1f0e5abfd52d1f8acac705d5ea1c367720
kazenetu <rakudafactory@gmail.com>
2019-07-26 06:11:46 +0900

    Testスーパークラスを継承する実装に変更

--- CStoTSTest/UnitTest1.cs
+++ CStoTSTest/UnitTest1.cs
@@ -1,38 +1,32 @@
 using CStoTS;
-using CStoTS.ApplicationService;
-using CStoTSTest.Common;
-using System.Linq;
 using System.Text;
 using Xunit;
 
 namespace CStoTSTest
 {
-  public class UnitTest1
+  public class UnitTest1: TestBase
   {
     [Fact]
     public void Test1()
     {
       // C#ソース作成
-      var testBase = new TestBase();
-      testBase.CreateFileData("test.cs", string.Empty,
+      CreateFileData("test.cs", string.Empty,
       @"public class Test
       {
       }");
 
       // 変換
-      var csToTs = new ConvertApplication();
-      var tsFiles = new TSFileRepositoryMock();
-      csToTs.Convert(string.Empty, string.Empty, tsFiles, testBase.Files);
+      ConvertTS();
 
       // 変換確認
-      var actual = tsFiles.Scripts.First();
-      Assert.Equal("test.ts", actual.filePath);
+      var actualTS = GetTypeScript("test.ts");
+      Assert.NotNull(actualTS);
 
-      var expectedScript = new StringBuilder();
-      expectedScript.AppendLine("class Test{");
-      expectedScript.AppendLine("}");
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("class Test{");
+      expectedTS.AppendLine("}");
 
-      Assert.Equal(expectedScript.ToString(), actual.typeScripts);
+      Assert.Equal(expectedTS.ToString(), actualTS);
     }
   }
 }

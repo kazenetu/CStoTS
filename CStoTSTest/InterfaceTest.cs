e8a67098d3dc1c7daf3c4ba45c96d62e3727b3ac
kazenetu <rakudafactory@gmail.com>
2019-08-03 20:56:48 +0900

    Interface変換クラスのテストコードを追加

---
+++ CStoTSTest/InterfaceTest.cs
@@ -0,0 +1,86 @@
+﻿using CStoTS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CStoTSTest
+{
+  public class InterfaceTest : TestBase
+  {
+    [Fact]
+    public void StandardTest()
+    {
+      // C#ソース作成
+      CreateFileData("test.cs", string.Empty,
+      @"public interface Test
+      {
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("test.ts");
+      Assert.NotNull(actualTS);
+
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("interface Test {");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+    [Fact]
+    public void GenericsTest()
+    {
+      // C#ソース作成
+      CreateFileData("test.cs", string.Empty,
+      @"public interface Test<T>
+      {
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("test.ts");
+      Assert.NotNull(actualTS);
+
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("interface Test<T> {");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+    [Fact]
+    public void GenericsSubFixedTypeTest()
+    {
+      // C#ソース作成
+      CreateFileData("test.cs", string.Empty,
+      @"public interface Test<T,U,V>
+      {
+      }");
+      CreateFileData("sub.cs", string.Empty,
+      @"
+      public interface Sub :Test<string, int, decimal>
+      {
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("sub.ts");
+      Assert.NotNull(actualTS);
+
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("interface Sub extends Test<String,Number,Number> {");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+  }
+}

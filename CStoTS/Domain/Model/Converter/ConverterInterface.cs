bac5779ebd20423a5564dbfaf37da0f5b84ab6b3
kazenetu <rakudafactory@gmail.com>
2019-08-03 20:46:14 +0900

    TS変換クラス：interface追加

---
+++ CStoTS/Domain/Model/Converter/ConverterInterface.cs
@@ -0,0 +1,35 @@
+﻿using CSharpAnalyze.Domain.PublicInterfaces;
+using CSharpAnalyze.Domain.PublicInterfaces.AnalyzeItems;
+using CStoTS.Domain.Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CStoTS.Domain.Model.Converter
+{
+  /// <summary>
+  /// TS変換クラス：interface
+  /// </summary>
+  internal class ConverterInterface : AbstractConverter, IConvertable
+  {
+    /// <summary>
+    /// エントリメソッド
+    /// </summary>
+    /// <param name="item">C#解析結果</param>
+    /// <param name="indent">インデント数</param>
+    /// <returns>TypeScript変換結果</returns>
+    public string Convert(IAnalyzeItem item, int indent)
+    {
+      return Convert(item as IItemInterface, indent);
+    }
+
+    public string Convert(IItemInterface item, int indent)
+    {
+      var result = new StringBuilder();
+
+      return result.ToString();
+    }
+
+  }
+}

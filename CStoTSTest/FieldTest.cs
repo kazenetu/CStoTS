3108c15e5cadd2838b9b99eb877d87df2b510f1a
kazenetu <rakudafactory@gmail.com>
2019-08-19 00:42:20 +0900

    フィールドテストの追加

---
+++ CStoTSTest/FieldTest.cs
@@ -0,0 +1,144 @@
+﻿using CStoTS;
+using System.Text;
+using Xunit;
+
+namespace CStoTSTest
+{
+  [Trait("フィールドのテスト", nameof(FieldTest))]
+  public class FieldTest : TestBase
+  {
+    [Fact(DisplayName = "StandardTest")]
+    public void StandardTest()
+    {
+      // C#ソース作成
+      CreateFileData("test.cs", string.Empty,
+      @"public class Test
+      {
+        public string field;
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("test.ts");
+      Assert.NotNull(actualTS);
+
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("export class Test {");
+      expectedTS.AppendLine("  public field: string;");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+    [Fact(DisplayName = "DefaultTest")]
+    public void DefaultTest()
+    {
+      // C#ソース作成
+      CreateFileData("test.cs", string.Empty,
+      @"public class Test
+      {
+        public string field = ""abc"";
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("test.ts");
+      Assert.NotNull(actualTS);
+
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("export class Test {");
+      expectedTS.AppendLine("  public field: string = \"abc\";");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+    [Fact(DisplayName = "PrivateTest")]
+    public void PrivateTest()
+    {
+      // C#ソース作成
+      CreateFileData("test.cs", string.Empty,
+      @"public class Test
+      {
+        private string field;
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("test.ts");
+      Assert.NotNull(actualTS);
+
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("export class Test {");
+      expectedTS.AppendLine("  private field: string;");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+    [Fact(DisplayName = "ProtectedTest")]
+    public void ProtectedTest()
+    {
+      // C#ソース作成
+      CreateFileData("test.cs", string.Empty,
+      @"public class Test
+      {
+        protected string field;
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("test.ts");
+      Assert.NotNull(actualTS);
+
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("export class Test {");
+      expectedTS.AppendLine("  protected field: string;");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+    [Fact(DisplayName = "MultiTest")]
+    public void MultiTest()
+    {
+      // C#ソース作成
+      CreateFileData("myclass.cs", string.Empty,
+      @"public class MyClass
+      {
+      }");
+      CreateFileData("test.cs", string.Empty,
+      @"public class Test
+      {
+        public string field1;
+        protected decimal field2;
+        private MyClass field3;
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("test.ts");
+      Assert.NotNull(actualTS);
+
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("export class Test {");
+      expectedTS.AppendLine("  public field1: string;");
+      expectedTS.AppendLine("  protected field2: number;");
+      expectedTS.AppendLine("  private field3: MyClass;");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+  }
+}

b55a7dd9b619b225a6ed7c33375e7e23eb875c82
kazenetu <rakudafactory@gmail.com>
2019-08-23 00:30:38 +0900

    returnのTypeScript変換クラスを追加

---
+++ CStoTS/Domain/Model/Converter/ConvertReturn.cs
@@ -0,0 +1,51 @@
+﻿using CSharpAnalyze.Domain.PublicInterfaces;
+using CSharpAnalyze.Domain.PublicInterfaces.AnalyzeItems;
+using CStoTS.Domain.Model.Interface;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CStoTS.Domain.Model.Converter
+{
+  /// <summary>
+  /// TS変換クラス：リターン
+  /// </summary>
+  internal　class ConvertReturn : AbstractConverter, IConvertable
+  {
+    /// <summary>
+    /// エントリメソッド
+    /// </summary>
+    /// <param name="item">C#解析結果</param>
+    /// <param name="indent">インデント数</param>
+    /// <returns>TypeScript変換結果</returns>
+    public string Convert(IAnalyzeItem item, int indent)
+    {
+      return Convert(item as IItemReturn, indent);
+    }
+
+    /// <summary>
+    /// 変換メソッド
+    /// </summary>
+    /// <param name="item">C#解析結果</param>
+    /// <param name="indent">インデント数</param>
+    /// <returns>TypeScript変換結果</returns>
+    private string Convert(IItemReturn item, int indent)
+    {
+      var result = new StringBuilder();
+      var indentSpace = GetIndentSpace(indent);
+
+      // コメント
+      result.Append(GetTypeScriptComments(item, indentSpace));
+
+      // 戻り値設定
+      var returnValue = ExpressionsToString(item.ReturnValue);
+      if (!string.IsNullOrEmpty(returnValue))
+      {
+        returnValue = " " + returnValue;
+      }
+      result.AppendLine($"{indentSpace}return{returnValue};");
+
+      return result.ToString();
+    }
+  }
+}

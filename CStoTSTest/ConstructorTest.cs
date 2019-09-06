09c01f773f18afcffbadeaa24724f3552a0804ba
kazenetu <rakudafactory@gmail.com>
2019-09-06 22:07:22 +0900

    多重定義のスコープ設定のテストを追加

--- CStoTSTest/ConstructorTest.cs
+++ CStoTSTest/ConstructorTest.cs
@@ -302,5 +302,75 @@ namespace CStoTSTest
       Assert.Equal(expectedTS.ToString(), actualTS);
     }
 
+    [Fact(DisplayName = "PrivateScopeOverloadTest")]
+    public void PrivateScopeOverloadTest()
+    {
+      // C#ソース作成
+      CreateFileData("test.cs", string.Empty,
+      @"public class Test
+      {
+        private Test()
+        {
+        }
+        private Test(int param1)
+        {
+        }
+        private Test(string param1)
+        {
+        }
+        private Test(int param1,string param2)
+        {
+        }
+        private Test(string param1,string param2,string param3)
+        {
+        }
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("test.ts");
+      Assert.NotNull(actualTS);
+
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("export class Test {");
+      expectedTS.AppendLine("  private constructor1() {");
+      expectedTS.AppendLine("  }");
+      expectedTS.AppendLine("  private constructor2(param1: number) {");
+      expectedTS.AppendLine("  }");
+      expectedTS.AppendLine("  private constructor3(param1: string) {");
+      expectedTS.AppendLine("  }");
+      expectedTS.AppendLine("  private constructor4(param1: number, param2: string) {");
+      expectedTS.AppendLine("  }");
+      expectedTS.AppendLine("  private constructor5(param1: string, param2: string, param3: string) {");
+      expectedTS.AppendLine("  }");
+      expectedTS.AppendLine("  private constructor(param1?: number | string, param2?: string, param3?: string) {");
+      expectedTS.AppendLine("    if (param1 === undefined && param2 === undefined && param3 === undefined) {");
+      expectedTS.AppendLine("      this.constructor1();");
+      expectedTS.AppendLine("      return;");
+      expectedTS.AppendLine("    }");
+      expectedTS.AppendLine("    if (typeof param1 === 'number' && param2 === undefined && param3 === undefined) {");
+      expectedTS.AppendLine("      this.constructor2(param1);");
+      expectedTS.AppendLine("      return;");
+      expectedTS.AppendLine("    }");
+      expectedTS.AppendLine("    if (typeof param1 === 'string' && param2 === undefined && param3 === undefined) {");
+      expectedTS.AppendLine("      this.constructor3(param1);");
+      expectedTS.AppendLine("      return;");
+      expectedTS.AppendLine("    }");
+      expectedTS.AppendLine("    if (typeof param1 === 'number' && typeof param2 === 'string' && param3 === undefined) {");
+      expectedTS.AppendLine("      this.constructor4(param1, param2);");
+      expectedTS.AppendLine("      return;");
+      expectedTS.AppendLine("    }");
+      expectedTS.AppendLine("    if (typeof param1 === 'string' && typeof param2 === 'string' && typeof param3 === 'string') {");
+      expectedTS.AppendLine("      this.constructor5(param1, param2, param3);");
+      expectedTS.AppendLine("      return;");
+      expectedTS.AppendLine("    }");
+      expectedTS.AppendLine("  }");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
   }
 }

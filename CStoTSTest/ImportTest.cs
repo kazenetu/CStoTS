2282989bb26ae654286a7076c6116758036e4a46
kazenetu <rakudafactory@gmail.com>
2019-09-11 00:00:32 +0900

    階層構造のあるimport(外部ファイル参照)のテストを追加

---
+++ CStoTSTest/ImportTest.cs
@@ -0,0 +1,220 @@
+﻿using CStoTS;
+using System.Text;
+using Xunit;
+
+namespace CStoTSTest
+{
+  [Trait("外部ファイル参照のテスト", nameof(ImportTest))]
+  public class ImportTest : TestBase
+  {
+    [Fact(DisplayName = "StandardTest")]
+    public void StandardTest()
+    {
+      // C#ソース作成
+      CreateFileData("test.cs", string.Empty,
+      @"public class Test
+      {
+      }");
+      CreateFileData("sub.cs", string.Empty,
+      @"public class Sub:Test
+      {
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("sub.ts");
+      Assert.NotNull(actualTS);
+
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("import { Test } from 'test';");
+      expectedTS.AppendLine("");
+      expectedTS.AppendLine("export class Sub extends Test {");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+    [Fact(DisplayName = "MultiReferenceTest")]
+    public void MultiReferenceTest()
+    {
+      // C#ソース作成
+      CreateFileData("test.cs", string.Empty,
+      @"public class Test
+      {
+      }
+      public class MyClass
+      {
+      }");
+      CreateFileData("sub.cs", string.Empty,
+      @"public class Sub:Test
+      {
+        private MyClass field;
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("sub.ts");
+      Assert.NotNull(actualTS);
+
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("import { Test, MyClass } from 'test';");
+      expectedTS.AppendLine("");
+      expectedTS.AppendLine("export class Sub extends Test {");
+      expectedTS.AppendLine("  private field: MyClass;");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+    [Fact(DisplayName = "OtherDirectoryTest")]
+    public void OtherDirectoryTest()
+    {
+      // C#ソース作成
+      CreateFileData("base/test.cs", string.Empty,
+      @"public class Test
+      {
+      }");
+      CreateFileData("sub.cs", string.Empty,
+      @"public class Sub:Test
+      {
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("sub.ts");
+      Assert.NotNull(actualTS);
+
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("import { Test } from 'base/test';");
+      expectedTS.AppendLine("");
+      expectedTS.AppendLine("export class Sub extends Test {");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+    [Fact(DisplayName = "OtherDirectoryReverseTest")]
+    public void OtherDirectoryReverseTest()
+    {
+      // C#ソース作成
+      CreateFileData("test.cs", string.Empty,
+      @"public class Test
+      {
+      }");
+      CreateFileData("sub/sub.cs", string.Empty,
+      @"public class Sub:Test
+      {
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("sub/sub.ts");
+      Assert.NotNull(actualTS);
+
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("import { Test } from '../test';");
+      expectedTS.AppendLine("");
+      expectedTS.AppendLine("export class Sub extends Test {");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+    [Fact(DisplayName = "DifferenceReferenceTest")]
+    public void DifferenceReferenceTest()
+    {
+      // C#ソース作成
+      CreateFileData("base/test.cs", string.Empty,
+      @"public class Test
+      {
+      }");
+      CreateFileData("sub/sub.cs", string.Empty,
+      @"public class Sub:Test
+      {
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("sub/sub.ts");
+      Assert.NotNull(actualTS);
+
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("import { Test } from '../base/test';");
+      expectedTS.AppendLine("");
+      expectedTS.AppendLine("export class Sub extends Test {");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+    [Fact(DisplayName = "EachReferenceTest")]
+    public void EachReferenceTest()
+    {
+      // C#ソース作成
+      CreateFileData("base/test.cs", string.Empty,
+      @"public class Test
+      {
+      }");
+      CreateFileData("base/sub/sub.cs", string.Empty,
+      @"public class Sub:Test
+      {
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("base/sub/sub.ts");
+      Assert.NotNull(actualTS);
+
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("import { Test } from '../test';");
+      expectedTS.AppendLine("");
+      expectedTS.AppendLine("export class Sub extends Test {");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+    [Fact(DisplayName = "CollectionTest")]
+    public void CollectionTest()
+    {
+      // C#ソース作成
+      CreateFileData("test.cs", string.Empty,
+      @"public class Test
+      {
+        private List<string> field1;
+        private Dictionary<int,string> field2;
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("test.ts");
+      Assert.NotNull(actualTS);
+
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("import { Dictionary } from 'Dictionary';");
+      expectedTS.AppendLine("import { List } from 'List';");
+      expectedTS.AppendLine("");
+      expectedTS.AppendLine("export class Sub extends Test {");
+      expectedTS.AppendLine("  private field1: List<string>;");
+      expectedTS.AppendLine("  private field2: Dictionary<number, string>;");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+  }
+}

400298c5379cbca0f979a4e026eecf017c9047fd
kazenetu <rakudafactory@gmail.com>
2019-09-18 20:56:35 +0900

    固定出力のTypeScriptファイルを追加

---
+++ CStoTS/Infrastructure/TypeScripts/JSONConverter.ts
@@ -0,0 +1,26 @@
+﻿/**
+ * Simple JSONConverter for TypeScript
+ * Copyright 2019, kazenetu
+ * Released under the MIT License.
+ */
+export class JSONConverter<T>
+{
+  public serialize(target: T): string {
+    return JSON.stringify(target);
+  }
+  public deserialize(jsonString: string, newInstance: T): T {
+    return this.deserializeMapping(newInstance, JSON.parse(jsonString));
+  }
+  private deserializeMapping(newInstance: any, jsonObject: any): T {
+    for (let itemName in jsonObject) {
+      let itemObject = jsonObject[itemName];
+
+      if (typeof itemObject === 'object') {
+        this.deserializeMapping(newInstance[itemName], itemObject);
+      } else {
+        newInstance[itemName] = jsonObject[itemName];
+      }
+    }
+    return newInstance;
+  }
+}

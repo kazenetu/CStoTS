424ceffaea9d9df43974ac7bd0974ec7edaae7d7
kazenetu <rakudafactory@gmail.com>
2019-10-01 19:58:26 +0900

    ローカルメソッドのテストを実装

--- CStoTSTest/LocalFunctionTest .cs
+++ CStoTSTest/LocalFunctionTest .cs
@@ -10,10 +10,29 @@ namespace CStoTSTest
     [Fact(DisplayName = "StandardTest")]
     public void StandardTest()
     {
-      // TODO C#ソース作成
+      // C#ソース作成
       CreateFileData("test.cs", string.Empty,
       @"public class Test
       {
+        public void Method()
+        {
+          void VoidMethod()
+          {
+          }
+          int IntMethod()
+          {
+            return 0;
+          }
+          string StringMethod()
+          {
+            return ""ABC"";
+          }
+
+          VoidMethod();
+          IntMethod();
+          StringMethod();
+          var intValue = IntMethod();
+        }
       }");
 
       // 変換
@@ -23,10 +42,136 @@ namespace CStoTSTest
       var actualTS = GetTypeScript("test.ts");
       Assert.NotNull(actualTS);
 
-      // TODO 変換後の期待値設定
+      // 変換後の期待値設定
       var expectedTS = new StringBuilder();
-      //expectedTS.AppendLine("export class Test {");
-      //expectedTS.AppendLine("}");
+      expectedTS.AppendLine("export class Test {");
+      expectedTS.AppendLine("  public Method(): void {");
+      expectedTS.AppendLine("    let VoidMethod = (): void => {");
+      expectedTS.AppendLine("    };");
+      expectedTS.AppendLine("    let IntMethod = (): number => {");
+      expectedTS.AppendLine("      return 0;");
+      expectedTS.AppendLine("    };");
+      expectedTS.AppendLine("    let StringMethod = (): string => {");
+      expectedTS.AppendLine("      return \"ABC\";");
+      expectedTS.AppendLine("    };");
+      expectedTS.AppendLine("    VoidMethod();");
+      expectedTS.AppendLine("    IntMethod();");
+      expectedTS.AppendLine("    StringMethod();");
+      expectedTS.AppendLine("    let intValue: number = IntMethod();");
+      expectedTS.AppendLine("  }");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+    [Fact(DisplayName = "DeclarationClassTest")]
+    public void DeclarationClassTest()
+    {
+      // C#ソース作成
+      CreateFileData("test.cs", string.Empty,
+      @"public class Test
+      {
+        public static void Method()
+        {
+          void VoidMethod()
+          {
+          }
+          VoidMethod();
+        }
+
+        public class Inner {
+          public static void Method()
+          {
+            void VoidMethod()
+            {
+            }
+            VoidMethod();
+          }
+        }
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("test.ts");
+      Assert.NotNull(actualTS);
+
+      // 変換後の期待値設定
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("export class Test {");
+      expectedTS.AppendLine("  public Method(): void {");
+      expectedTS.AppendLine("    let VoidMethod = (): void => {");
+      expectedTS.AppendLine("    };");
+      expectedTS.AppendLine("    VoidMethod();");
+      expectedTS.AppendLine("  }");
+      expectedTS.AppendLine("}");
+
+      expectedTS.AppendLine("export namespace Test {");
+      expectedTS.AppendLine("  export class Inner {");
+      expectedTS.AppendLine("    public Method(): void {");
+      expectedTS.AppendLine("      let VoidMethod = (): void => {");
+      expectedTS.AppendLine("      };");
+      expectedTS.AppendLine("      VoidMethod();");
+      expectedTS.AppendLine("    }");
+      expectedTS.AppendLine("  }");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+    [Fact(DisplayName = "DeclarationEnumTest")]
+    public void DeclarationEnumTest()
+    {
+      // C#ソース作成
+      CreateFileData("test.cs", string.Empty,
+        @"public class Test
+      {
+        private enum CompassDirection
+        {
+          North,
+          East,
+          South,
+          West
+        }
+
+        public void Method()
+        {
+          CompassDirection EnumMethod()
+          {
+            return CompassDirection.East;
+          }
+          EnumMethod();
+          var enumValue = EnumMethod();
+        }
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("test.ts");
+      Assert.NotNull(actualTS);
+
+      // 変換後の期待値設定
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("export class Test {");
+      expectedTS.AppendLine("  public Method(): void {");
+      expectedTS.AppendLine("    let EnumMethod = (): CompassDirection => {");
+      expectedTS.AppendLine("      return CompassDirection.East;");
+      expectedTS.AppendLine("    };");
+      expectedTS.AppendLine("    EnumMethod();");
+      expectedTS.AppendLine("    let enumValue: CompassDirection = EnumMethod();");
+      expectedTS.AppendLine("  }");
+      expectedTS.AppendLine("}");
+      expectedTS.AppendLine("export namespace Test {");
+      expectedTS.AppendLine("  export enum CompassDirection {");
+      expectedTS.AppendLine("    North,");
+      expectedTS.AppendLine("    East,");
+      expectedTS.AppendLine("    South,");
+      expectedTS.AppendLine("    West,");
+      expectedTS.AppendLine("  }");
+      expectedTS.AppendLine("}");
 
       Assert.Equal(expectedTS.ToString(), actualTS);
     }

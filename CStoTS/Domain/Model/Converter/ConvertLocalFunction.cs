7acacc1cb8bfcc5e417e4d5c6652fc3d9177263e
kazenetu <rakudafactory@gmail.com>
2019-10-02 18:41:29 +0900

    ローカルメソッドの変換処理の実装

---
+++ CStoTS/Domain/Model/Converter/ConvertLocalFunction.cs
@@ -0,0 +1,78 @@
+﻿using CSharpAnalyze.Domain.PublicInterfaces;
+using CSharpAnalyze.Domain.PublicInterfaces.AnalyzeItems;
+using CStoTS.Domain.Model.Interface;
+using CStoTS.Domain.Model.Mode;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CStoTS.Domain.Model.Converter
+{
+  /// <summary>
+  /// TS変換クラス：ローカルメソッド
+  /// </summary>
+  internal class ConvertLocalFunction : AbstractConverter, IConvertable
+  {
+    /// <summary>
+    /// エントリメソッド
+    /// </summary>
+    /// <param name="item">C#解析結果</param>
+    /// <param name="config">設定情報</param>
+    /// <param name="indent">インデント数</param>
+    /// <param name="otherScripts">その他のスクリプト(内部クラスなど)</param>
+    /// <returns>TypeScript変換結果</returns>
+    public string Convert(IAnalyzeItem item, Config config, int indent, List<string> otherScripts)
+    {
+      return Convert(item as IItemLocalFunction, config, indent, otherScripts);
+    }
+
+    /// <summary>
+    /// 変換メソッド
+    /// </summary>
+    /// <param name="item">C#解析結果</param>
+    /// <param name="config">設定情報</param>
+    /// <param name="indent">インデント数</param>
+    /// <param name="otherScripts">その他のスクリプト(内部クラスなど)</param>
+    /// <returns>TypeScript変換結果</returns>
+    private string Convert(IItemLocalFunction item, Config config, int indent, List<string> otherScripts)
+    {
+      var result = new StringBuilder();
+      var indentSpace = GetIndentSpace(indent);
+
+      // コメント
+      result.Append(GetTypeScriptComments(item, indentSpace));
+
+      // パラメータ取得
+      var args = new List<string>();
+      foreach (var arg in item.Args)
+      {
+        args.Add($"{arg.name}: {ExpressionsToString(arg.expressions)}");
+      }
+
+      // ジェネリックスクラス
+      var genericTypes = new StringBuilder();
+      if (item.GenericTypes.Any())
+      {
+        genericTypes.Append("<");
+        genericTypes.Append(string.Join(", ", item.GenericTypes.Select(typeItem => GetTypeScriptType(typeItem))));
+        genericTypes.Append(">");
+      }
+
+      // 定義
+      result.Append($"{indentSpace}let {item.Name} = {genericTypes.ToString()}(");
+      result.Append(string.Join(", ", args));
+      result.AppendLine($"): {ExpressionsToString(item.MethodTypes)} => {{");
+
+      // メンバー追加
+      foreach (var member in item.Members)
+      {
+        result.Append(ConvertUtility.Convert(member, config, indent + 1, otherScripts));
+      }
+
+      result.AppendLine($"{indentSpace}}};");
+
+      return result.ToString();
+    }
+
+  }
+}

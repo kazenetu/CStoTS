3e20603678e074ad15c0b7c5f42324ff08bdd87f
kazenetu <rakudafactory@gmail.com>
2019-10-06 16:15:04 +0900

    インデントスペースを4から2に変更

--- CStoTS/Domain/Model/Converter/ConvertIfElse.cs
+++ CStoTS/Domain/Model/Converter/ConvertIfElse.cs
@@ -8,104 +8,104 @@ using System.Text;
 
 namespace CStoTS.Domain.Model.Converter
 {
+  /// <summary>
+  /// TS変換クラス：条件分岐
+  /// </summary>
+  internal class ConvertIfElse : AbstractConverter, IConvertable
+  {
     /// <summary>
-    /// TS変換クラス：条件分岐
+    /// エントリメソッド
     /// </summary>
-    internal class ConvertIfElse : AbstractConverter, IConvertable
+    /// <param name="item">C#解析結果</param>
+    /// <param name="config">設定情報</param>
+    /// <param name="indent">インデント数</param>
+    /// <param name="otherScripts">その他のスクリプト(内部クラスなど)</param>
+    /// <returns>TypeScript変換結果</returns>
+    public string Convert(IAnalyzeItem item, Config config, int indent, List<string> otherScripts)
     {
-        /// <summary>
-        /// エントリメソッド
-        /// </summary>
-        /// <param name="item">C#解析結果</param>
-        /// <param name="config">設定情報</param>
-        /// <param name="indent">インデント数</param>
-        /// <param name="otherScripts">その他のスクリプト(内部クラスなど)</param>
-        /// <returns>TypeScript変換結果</returns>
-        public string Convert(IAnalyzeItem item, Config config, int indent, List<string> otherScripts)
-        {
 
-            if (item is IItemIf)
-            {
-                return Convert(item as IItemIf, config, indent, otherScripts);
-            }
-            else
-            {
-                return Convert(item as IItemElseClause, config, indent, otherScripts);
-            }
-        }
-
-        /// <summary>
-        /// 変換メソッド:if
-        /// </summary>
-        /// <param name="item">C#解析結果</param>
-        /// <param name="config">設定情報</param>
-        /// <param name="indent">インデント数</param>
-        /// <param name="otherScripts">その他のスクリプト(内部クラスなど)</param>
-        /// <returns>TypeScript変換結果</returns>
-        private string Convert(IItemIf item, Config config, int indent, List<string> otherScripts)
-        {
-            var result = new StringBuilder();
-            var indentSpace = GetIndentSpace(indent);
-
-            // コメント
-            result.Append(GetTypeScriptComments(item, indentSpace));
-
-            // 定義
-            result.AppendLine($"{indentSpace}if ({ExpressionsToString(item.Conditions)}) {{");
-
-            // メンバー追加
-            foreach (var member in item.TrueBlock)
-            {
-                result.Append(ConvertUtility.Convert(member, config, indent + 1, otherScripts));
-            }
-
-            result.AppendLine($"{indentSpace}}}");
-
-            // else構文追加
-            foreach (var block in item.FalseBlocks)
-            {
-                result.Append(ConvertUtility.Convert(block, config, indent, otherScripts));
-            }
-
-            return result.ToString();
-        }
-
-        /// <summary>
-        /// 変換メソッド:else
-        /// </summary>
-        /// <param name="item">C#解析結果</param>
-        /// <param name="config">設定情報</param>
-        /// <param name="indent">インデント数</param>
-        /// <param name="otherScripts">その他のスクリプト(内部クラスなど)</param>
-        /// <returns>TypeScript変換結果</returns>
-        private string Convert(IItemElseClause item, Config config, int indent, List<string> otherScripts)
-        {
-            var result = new StringBuilder();
-            var indentSpace = GetIndentSpace(indent);
-
-            // コメント
-            result.Append(GetTypeScriptComments(item, indentSpace));
-
-            // 定義
-            if (item.Conditions.Any())
-            {
-                result.AppendLine($"{indentSpace}else if ({ExpressionsToString(item.Conditions)}) {{");
-            }
-            else
-            {
-                result.AppendLine($"{indentSpace}else {{");
-            }
-
-            // メンバー追加
-            foreach (var member in item.Block)
-            {
-                result.Append(ConvertUtility.Convert(member, config, indent + 1, otherScripts));
-            }
-
-            result.AppendLine($"{indentSpace}}}");
-
-            return result.ToString();
-        }
-
+      if (item is IItemIf)
+      {
+        return Convert(item as IItemIf, config, indent, otherScripts);
+      }
+      else
+      {
+        return Convert(item as IItemElseClause, config, indent, otherScripts);
+      }
     }
+
+    /// <summary>
+    /// 変換メソッド:if
+    /// </summary>
+    /// <param name="item">C#解析結果</param>
+    /// <param name="config">設定情報</param>
+    /// <param name="indent">インデント数</param>
+    /// <param name="otherScripts">その他のスクリプト(内部クラスなど)</param>
+    /// <returns>TypeScript変換結果</returns>
+    private string Convert(IItemIf item, Config config, int indent, List<string> otherScripts)
+    {
+      var result = new StringBuilder();
+      var indentSpace = GetIndentSpace(indent);
+
+      // コメント
+      result.Append(GetTypeScriptComments(item, indentSpace));
+
+      // 定義
+      result.AppendLine($"{indentSpace}if ({ExpressionsToString(item.Conditions)}) {{");
+
+      // メンバー追加
+      foreach (var member in item.TrueBlock)
+      {
+        result.Append(ConvertUtility.Convert(member, config, indent + 1, otherScripts));
+      }
+
+      result.AppendLine($"{indentSpace}}}");
+
+      // else構文追加
+      foreach (var block in item.FalseBlocks)
+      {
+        result.Append(ConvertUtility.Convert(block, config, indent, otherScripts));
+      }
+
+      return result.ToString();
+    }
+
+    /// <summary>
+    /// 変換メソッド:else
+    /// </summary>
+    /// <param name="item">C#解析結果</param>
+    /// <param name="config">設定情報</param>
+    /// <param name="indent">インデント数</param>
+    /// <param name="otherScripts">その他のスクリプト(内部クラスなど)</param>
+    /// <returns>TypeScript変換結果</returns>
+    private string Convert(IItemElseClause item, Config config, int indent, List<string> otherScripts)
+    {
+      var result = new StringBuilder();
+      var indentSpace = GetIndentSpace(indent);
+
+      // コメント
+      result.Append(GetTypeScriptComments(item, indentSpace));
+
+      // 定義
+      if (item.Conditions.Any())
+      {
+        result.AppendLine($"{indentSpace}else if ({ExpressionsToString(item.Conditions)}) {{");
+      }
+      else
+      {
+        result.AppendLine($"{indentSpace}else {{");
+      }
+
+      // メンバー追加
+      foreach (var member in item.Block)
+      {
+        result.Append(ConvertUtility.Convert(member, config, indent + 1, otherScripts));
+      }
+
+      result.AppendLine($"{indentSpace}}}");
+
+      return result.ToString();
+    }
+
+  }
 }

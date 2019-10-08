03c4b20584475b83c2c090bb277526e6d16fae93
kazenetu <rakudafactory@gmail.com>
2019-10-09 00:07:05 +0900

    for文のテストコードを実装

--- CStoTSTest/ForTest.cs
+++ CStoTSTest/ForTest.cs
@@ -10,10 +10,15 @@ namespace CStoTSTest
     [Fact(DisplayName = "StandardTest")]
     public void StandardTest()
     {
-      // TODO C#ソース作成
+      // C#ソース作成
       CreateFileData("test.cs", string.Empty,
       @"public class Test
       {
+        public void Method(){
+          for(var i=0;i<10;i++)
+          {
+          }
+        }
       }");
 
       // 変換
@@ -23,10 +28,92 @@ namespace CStoTSTest
       var actualTS = GetTypeScript("test.ts");
       Assert.NotNull(actualTS);
 
-      // TODO 変換後の期待値設定
+      // 変換後の期待値設定
       var expectedTS = new StringBuilder();
-      //expectedTS.AppendLine("export class Test {");
-      //expectedTS.AppendLine("}");
+      expectedTS.AppendLine("export class Test {");
+      expectedTS.AppendLine("  public Method(): void {");
+      expectedTS.AppendLine("    for (let i: number = 0; i < 10; i++) {");
+      expectedTS.AppendLine("    }");
+      expectedTS.AppendLine("  }");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+    [Fact(DisplayName = "GenericsTest")]
+    public void GenericsTest()
+    {
+      // C#ソース作成
+      CreateFileData("test.cs", string.Empty,
+      @"public class Test
+      {
+        public void Method(){
+          var list = new List<string>();
+          list.Add(""A"");
+          list.Add(""B"");
+          list.Add(""C"");
+          for(var i=0;i<list.Count;i++)
+          {
+          }
+        }
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("test.ts");
+      Assert.NotNull(actualTS);
+
+      // 変換後の期待値設定(List.tsは別途出力されるArrayラッパークラス)
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("import { List } from './List';");
+      expectedTS.AppendLine("");
+      expectedTS.AppendLine("export class Test {");
+      expectedTS.AppendLine("  public Method(): void {");
+      expectedTS.AppendLine("    let list: List = new List<string>();");
+      expectedTS.AppendLine("    list.Add(\"A\");");
+      expectedTS.AppendLine("    list.Add(\"B\");");
+      expectedTS.AppendLine("    list.Add(\"C\");");
+      expectedTS.AppendLine("    for (let i: number = 0; i < list.Count; i++) {");
+      expectedTS.AppendLine("    }");
+      expectedTS.AppendLine("  }");
+      expectedTS.AppendLine("}");
+
+      Assert.Equal(expectedTS.ToString(), actualTS);
+    }
+
+    [Fact(DisplayName = "ArrayTest")]
+    public void ArrayTest()
+    {
+      // C#ソース作成
+      CreateFileData("test.cs", string.Empty,
+      @"public class Test
+      {
+        public void Method(){
+          int[] list = { 1, 2, 3 };
+          for (var i = 0; i < list.Length; i++)
+          {
+          }
+        }
+      }");
+
+      // 変換
+      ConvertTS();
+
+      // 変換確認
+      var actualTS = GetTypeScript("test.ts");
+      Assert.NotNull(actualTS);
+
+      // 変換後の期待値設定
+      var expectedTS = new StringBuilder();
+      expectedTS.AppendLine("export class Test {");
+      expectedTS.AppendLine("  public Method(): void {");
+      expectedTS.AppendLine("    let list:number[] = [1, 2, 3];");
+      expectedTS.AppendLine("    for (let i: number = 0; i < list.length; i++) {");
+      expectedTS.AppendLine("    }");
+      expectedTS.AppendLine("  }");
+      expectedTS.AppendLine("}");
 
       Assert.Equal(expectedTS.ToString(), actualTS);
     }

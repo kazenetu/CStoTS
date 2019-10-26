41d82e394fba18aed482fd6a4ac71ca41796682b
kazenetu <rakudafactory@gmail.com>
2019-10-27 00:41:12 +0900

    コンソールアプリの追加

---
+++ ConvertCStoTS/Common/ArgManagers.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections.Generic;
+
+namespace ConvertCStoTS.Common
+{
+  /// <summary>
+  /// パラメータ管理クラス
+  /// </summary>
+  public class ArgManagers
+  {
+    /// <summary>
+    /// オプションパラメータ(名前あり)のコレクション
+    /// </summary>
+    private Dictionary<string, string> OptionPramArgs = new Dictionary<string, string>();
+
+    /// <summary>
+    /// 必須パラメーター(名前なし)のコレクション
+    /// </summary>
+    private List<string> RequiredPramArgs = new List<string>();
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="args">パラメータ</param>
+    /// <remarks>パラメータのリストを作成する</remarks>
+    public ArgManagers(string[] args)
+    {
+      var paramName = string.Empty;
+      foreach (var arg in args)
+      {
+        switch (arg[0])
+        {
+          case '-':
+          case '/':
+            if (!string.IsNullOrEmpty(paramName))
+            {
+              // パラメータ名だけの場合はパラメータ名だけ追加
+              OptionPramArgs.Add(paramName, null);
+            }
+
+            // パラメータ名を設定
+            paramName = arg.Substring(0, 1).Replace("/", "-", System.StringComparison.CurrentCulture) +
+                        arg.Substring(1);
+            break;
+          default:
+
+            // パラメータの追加
+            if (string.IsNullOrEmpty(paramName))
+            {
+              // パラメータ名なし
+              RequiredPramArgs.Add(arg);
+            }
+            else
+            {
+              // パラメータ名あり
+              OptionPramArgs.Add(paramName, arg);
+            }
+
+            // パラメータ名をクリア
+            paramName = string.Empty;
+            break;
+        }
+      }
+
+      // パラメータ名だけの場合はパラメータ名だけ追加
+      if (!string.IsNullOrEmpty(paramName))
+      {
+        OptionPramArgs.Add(paramName, null);
+      }
+    }
+
+    /// <summary>
+    /// 必須パラメータの取得
+    /// </summary>
+    /// <param name="index">取得パラメータのインデックス</param>
+    /// <returns>対象パラメータの値(インデックスが存在しない場合はnull)</returns>
+    public string GetRequiredArg(int index)
+    {
+      if (RequiredPramArgs.Count <= index)
+      {
+        return null;
+      }
+
+      return RequiredPramArgs[index];
+    }
+
+    /// <summary>
+    /// 必須パラメータ数の取得
+    /// </summary>
+    /// <returns>必須パラメータ数</returns>
+    public int GetRequiredArgCount()
+    {
+      return RequiredPramArgs.Count;
+    }
+
+    /// <summary>
+    /// オプションパラメータの取得
+    /// </summary>
+    /// <param name="paramName">パラメータ名</param>
+    /// <returns>対象パラメータの値(パラメータ名が存在しない場合はnull)</returns>
+    public string GetOptionArg(string paramName)
+    {
+      if (!OptionPramArgs.ContainsKey(paramName))
+      {
+        return null;
+      }
+
+      return OptionPramArgs[paramName];
+    }
+
+    /// <summary>
+    /// オプションパラメータの取得
+    /// </summary>
+    /// <param name="paramName">パラメータ名リスト</param>
+    /// <returns>対象パラメータの値(パラメータ名が存在しない場合はnull)</returns>
+    public string GetOptionArg(List<string> paramNames)
+    {
+      string result = null;
+      foreach (var pramName in paramNames)
+      {
+        result = GetOptionArg(pramName);
+        if (result != null)
+        {
+          break;
+        }
+      }
+      return result;
+    }
+
+    /// <summary>
+    /// オプションパラメータ名の存在確認
+    /// </summary>
+    /// <param name="paramName">パラメータ名</param>
+    /// <returns>確認結果</returns>
+    public bool ExistsOptionArg(string paramName)
+    {
+      return OptionPramArgs.ContainsKey(paramName);
+    }
+
+    /// <summary>
+    /// オプションパラメータ名の存在確認
+    /// </summary>
+    /// <param name="paramName">パラメータ名リスト</param>
+    /// <returns>確認結果</returns>
+    public bool ExistsOptionArg(List<string> paramNames)
+    {
+      var result = false;
+      foreach (var pramName in paramNames)
+      {
+        result = ExistsOptionArg(pramName);
+        if (result)
+        {
+          break;
+        }
+      }
+      return result;
+    }
+  }
+}
---
+++ ConvertCStoTS/Program.cs
@@ -0,0 +1,93 @@
+﻿using ConvertCStoTS.Common;
+using CStoTS.ApplicationService;
+using CStoTS.Domain.Model.Mode;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConvertCStoTS
+{
+  class Program
+  {
+    static int Main(string[] args)
+    {
+      // パラメータ取得
+      var argManager = new ArgManagers(args);
+
+      // ヘルプモードの確認
+      var isShowHelp = false;
+      if (argManager.GetRequiredArgCount() <= 0)
+      {
+        // パラメータが不正の場合はヘルプモード
+        isShowHelp = true;
+      }
+      if (argManager.ExistsOptionArg(new List<string>() { "--help", "-h" }))
+      {
+        // ヘルプオプションはヘルプモード
+        isShowHelp = true;
+      }
+
+      // ヘルプ画面を表示
+      if (isShowHelp)
+      {
+        Console.WriteLine();
+        Console.WriteLine("how to use: ConvertCStoTS <SourcePath> [options]");
+        Console.WriteLine("");
+        Console.WriteLine("<SourcePath> Input C# Path");
+        Console.WriteLine("");
+        Console.WriteLine("options:");
+        Console.WriteLine("-f, --file  <FilePath>       Input C# Path");
+        Console.WriteLine("-o, --out   <OutputPath>     Output TypeScript Path");
+        Console.WriteLine("--no_method_output           No Method Output");
+        Console.WriteLine("-h, --help  view this page");
+        Console.WriteLine();
+        return 0;
+      }
+
+      var srcPath = Path.GetFullPath(argManager.GetRequiredArg(0));
+      var destPath = argManager.GetOptionArg(new List<string>() { "--out", "-o" });
+      if (string.IsNullOrEmpty(destPath))
+      {
+        destPath = Path.Combine(srcPath, "dest");
+      }
+      else
+      {
+        destPath = Path.GetFullPath(destPath);
+      }
+
+      // FilePath
+      var filePath = argManager.GetOptionArg(new List<string>() { "--file", " -f" });
+
+      // Output Method
+      var isOutputMethod = !argManager.ExistsOptionArg("--no_method_output");
+
+      try
+      {
+        // C#ファイルの変換とファイル出力
+        Console.WriteLine("---Convert Start---");
+        var csToTs = new ConvertApplication();
+        var mode = OutputMode.Mode.All;
+        if (isOutputMethod)
+        {
+          mode = OutputMode.Mode.WithoutMethod;
+        }
+        csToTs.Convert(Config.Create(mode,srcPath, destPath));
+
+        Console.WriteLine("---Convert End---");
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+        Console.WriteLine("---Convert Fail---");
+
+        return 1;
+      }
+
+#if DEBUG
+      Console.ReadKey();
+#endif
+
+      return 0;
+    }
+  }
+}

c9609805b3a3e288e7eb65a9f737cadc59f14e2d
kazenetu <rakudafactory@gmail.com>
2019-10-27 15:41:12 +0900

    C#のファイル指定ができるように拡張

--- CStoTS/Domain/Model/Mode/Config.cs
+++ CStoTS/Domain/Model/Mode/Config.cs
@@ -16,6 +16,11 @@
     public RootPath InputCSRoot { get; private set; }
 
     /// <summary>
+    /// 入力：C#のファイル指定
+    /// </summary>
+    public string InputFile { get; private set; }
+
+    /// <summary>
     /// 出力：TypeScriptのルートパス
     /// </summary>
     public RootPath OutputTSRoot { get; private set; }
@@ -25,11 +30,13 @@
     /// </summary>
     /// <param name="mode">出力モード</param>
     /// <param name="inputCSRoot">入力：C#のルートパス</param>
+    /// <param name="inputFile">出力：TypeScriptのルートパス</param>
     /// <param name="outputTSRoot">出力：TypeScriptのルートパス</param>
-    private Config(OutputMode mode, RootPath inputCSRoot, RootPath outputTSRoot)
+    private Config(OutputMode mode, RootPath inputCSRoot, RootPath outputTSRoot, string inputFile)
     {
       Mode = mode;
       InputCSRoot = inputCSRoot;
+      InputFile = inputFile;
       OutputTSRoot = outputTSRoot;
     }
 
@@ -38,10 +45,11 @@
     /// </summary>
     /// <param name="inputCSRoot">入力：C#のルートパス</param>
     /// <param name="outputTSRoot">出力：TypeScriptのルートパス</param>
+    /// <param name="inputFile">出力：TypeScriptのルートパス</param>
     /// <returns></returns>
-    public static Config Create(string inputCSRoot, string outputTSRoot)
+    public static Config Create(string inputCSRoot, string outputTSRoot, string inputFile = null)
     {
-      return new Config(OutputMode.Create(OutputMode.Mode.All), RootPath.Create(inputCSRoot), RootPath.Create(outputTSRoot));
+      return new Config(OutputMode.Create(OutputMode.Mode.All), RootPath.Create(inputCSRoot), RootPath.Create(outputTSRoot), inputFile);
     }
 
     /// <summary>
@@ -50,10 +58,11 @@
     /// <param name="mode">出力モード</param>
     /// <param name="inputCSRoot">入力：C#のルートパス</param>
     /// <param name="outputTSRoot">出力：TypeScriptのルートパス</param>
+    /// <param name="inputFile">出力：TypeScriptのルートパス</param>
     /// <returns></returns>
-    public static Config Create(OutputMode.Mode mode, string inputCSRoot, string outputTSRoot)
+    public static Config Create(OutputMode.Mode mode, string inputCSRoot, string outputTSRoot, string inputFile = null)
     {
-      return new Config(OutputMode.Create(mode), RootPath.Create(inputCSRoot), RootPath.Create(outputTSRoot));
+      return new Config(OutputMode.Create(mode), RootPath.Create(inputCSRoot), RootPath.Create(outputTSRoot), inputFile);
     }
   }
 }
